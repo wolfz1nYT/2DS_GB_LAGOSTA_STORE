526821c52bfa742c4709640f2bb9641c4d6ef0d6
Kalebe_j.s <wolfzin304@gmail.com>
2026-05-19 08:12:08 -0300

    Implement login verification function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-function verificarLogin()
+function verificarLogin() {
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+    
+    if (login === "admin" && senha === "123456") {
+        alert("acesso liberado!");
+        window.location.href="produtos.html";
+    } else {
+        alert("acesso negado!")
+    }
+
+    login = document.getElementById("login").value = "";
+    senha = document.getElementById("senha").value = "";
+
+
+
+
+
+}
